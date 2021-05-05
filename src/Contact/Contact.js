ad6514bacbc28567ca7cf7d6874df154bb58cde0
yl1462 <yliang1462@hotmail.com>
2021-05-04 20:21:03 -0500

    added more to contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,6 +8,14 @@ function Contact() {
   }
   return (
     <div>
+
+      <div className="intro">
+      <h2>Contact Yuri</h2><br />
+      <p>Thank you so much for clicking on my portfolio page! I hope you are liking what you are seeing so far!</p>
+      <p>Feel free to browse around my LinkedIn page, Github repos or just send me an email.</p>
+      <p>I wish you a wonderful day!</p><br/>
+      </div>
+      
       <div className="contactLinks">
         <ul>
           <li><a href="https://www.linkedin.com/in/weirui-yuri-liang" target="_blank" rel="noreferrer"
@@ -16,7 +24,7 @@ function Contact() {
           </li>
           <li><a href="mailto:yliang1462@hotmail.com" target="_blank" rel="noreferrer">yliang1462@hotmail.com</a></li>
         </ul>
-      </div>
+      </div><br />
 
       <form onSubmit={handleSubmit} className="form">
         <label htmlFor="email">
